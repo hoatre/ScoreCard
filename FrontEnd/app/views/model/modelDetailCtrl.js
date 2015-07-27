96a764bf9bc1598c299bfc8711b782092032d6e0
Chiennb <chiennb@gmail.com>
2015-07-27 12:09:13 +0700

    Add commit

--- FrontEnd/app/views/model/modelDetailCtrl.js
+++ FrontEnd/app/views/model/modelDetailCtrl.js
@@ -17,91 +17,85 @@
         if ($scope.model._id == undefined && $stateParams.modelId != '' && $stateParams.modelId != undefined) {
             $http.post(appSettings.serverPath + "/modelinfo/getbymodelinfoid", { _id: $stateParams.modelId })
                 .success(function (data) {
-                    console.log(data);
                     $scope.model = data.getModelInfoByIdJSON.body[0];
-                    console.log($scope.model);
                 });
         }
 
-        $scope.checkPublish = function (modelId) {
+        $scope.publish = function () {
+
+            if ($scope.model.status == 'draft') {
+                $scope.model.status = 'publish';
+            }
+            else {
+                $scope.model.status = 'draft';
+            }
             var strError = '';
-            $http.post(appSettings.serverPath + "/validate/checkweightrate", { modelid: $scope.model._id }).
-                    success(function (data, status, headers, config) {
-                        //console.log('data.checkweightrate.header.code');
-                        var code = data.checkweightrate.header.code;
-                        
-                        if (code == 0) {                            
-                            strError = '';
-                        }
-                        else if (code == 1) {
-                            strError = data.checkweightrate.header.message + ":\n";
-                            for (var i = 0; i < data.checkweightrate.body.weight.length; i++) {
-                                strError += "- " + data.checkweightrate.body.weight[i].FactorName + " : " + data.checkweightrate.body.weight[i].Weight + "\n";
+            if ($scope.model.status != 'draft') {
+                $http.post(appSettings.serverPath + "/validate/checkweightrate", { modelid: $scope.model._id }).
+                        success(function (data, status, headers, config) {
+                            //console.log('data.checkweightrate.header.code');
+                            var code = data.checkweightrate.header.code;
+
+                            if (code == 0) {
+                                $scope.save();
                             }
-                            strError += "------------------------------------------\n";
-                            strError += "- " + data.checkweightrate.body.rate;
-                            console.log(strError);                            
-                        }
-                        else if (code == 2) {
-                            strError = data.checkweightrate.header.message + ":\n";
-                            for (var i = 0; i < data.checkweightrate.body.weight.length; i++) {
-                                strError += "- " + data.checkweightrate.body.weight[i].FactorName + " : " + data.checkweightrate.body.weight[i].Weight + "\n";
+                            else if (code == 1) {
+                                strError = data.checkweightrate.header.message + ":\n";
+                                for (var i = 0; i < data.checkweightrate.body.weight.length; i++) {
+                                    strError += "- " + data.checkweightrate.body.weight[i].FactorName + " : " + data.checkweightrate.body.weight[i].Weight + "\n";
+                                }
+                                strError += "------------------------------------------\n";
+                                strError += "- " + data.checkweightrate.body.rate;
+                                //console.log(strError);
                             }
-                            console.log(strError);
-                            //$scope.statuscheck = false;
-                        }
-                        else if (code == 3) {
-                            strError = data.checkweightrate.header.message + ":\n";
-                            strError += "- " + data.checkweightrate.body.rate;
-                            console.log(strError);
-                            //$scope.statuscheck = false;
-                        }
-                        else if (code == 4) {
-                            strError = data.checkweightrate.header.message + ":\n";
-                            console.log(strError);
-                            //$scope.statuscheck = false;
-                        }
-                        
-                        return strError;
-                    }).
-                    error(function (data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                        //$scope.statuscheck = false
-                        return 'Error';
-                    });
+                            else if (code == 2) {
+                                strError = data.checkweightrate.header.message + ":\n";
+                                for (var i = 0; i < data.checkweightrate.body.weight.length; i++) {
+                                    strError += "- " + data.checkweightrate.body.weight[i].FactorName + " : " + data.checkweightrate.body.weight[i].Weight + "\n";
+                                }
+                                //console.log(strError);
+                                //$scope.statuscheck = false;
+                            }
+                            else if (code == 3) {
+                                strError = data.checkweightrate.header.message + ":\n";
+                                strError += "- " + data.checkweightrate.body.rate;
+                                //console.log(strError);
+                                //$scope.statuscheck = false;
+                            }
+                            else if (code == 4) {
+                                strError = data.checkweightrate.header.message + ":\n";
+                                //console.log(strError);
+                                //$scope.statuscheck = false;
+                            }
+                            else {
+                                strError = data.checkweightrate.header.message;
+                            }
+
+                            return strError;
+                        }).
+                        error(function (data, status, headers, config) {
+                            // called asynchronously if an error occurs
+                            // or server returns response with an error status.
+                            //$scope.statuscheck = false
+                            return 'Error';
+                        });
+            }
+            else {
+                $scope.save();
+            }
         }
 
         // Sava data
-        $scope.publish = function () {
+        $scope.save = function () {
 
-            if ($scope.model.status != 'draft') {
-                var strMessage = $scope.checkPublish($scope._id);
-
-                if (strMessage == '') {
-                    $http.post(appSettings.serverPath + "/modelinfo/update", $scope.model).
-                    success(function (data, status, headers, config) {
-                        popupService.showMessage('Publish Success!');
-                    }).
-                    error(function (data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                    });
-                }
-                else {
-                    popupService.showMessage(strMessage);
-                }
-            }
-            else {
-                $http.post(appSettings.serverPath + "/modelinfo/update", $scope.model).
-                    success(function (data, status, headers, config) {
-                        popupService.showMessage('Update Success!');
-                    }).
-                    error(function (data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                    });
-            }
+            $http.post(appSettings.serverPath + "/modelinfo/update", $scope.model).
+                success(function (data, status, headers, config) {
+                    popupService.showMessage('Update Success!');
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
         }
 
         // Back to list
