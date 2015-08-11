bc5465763a4cab84e2b39d3b7ec4710c2d1cfa37
phonghh2 <phonghh1989@gmail.com>
2015-08-11 16:36:59 +0700

    up dashboard

--- FrontEnd/app/views/dashboard/dashboar2Ctrl.js
+++ FrontEnd/app/views/dashboard/dashboar2Ctrl.js
@@ -7,28 +7,56 @@
                      Dashboard2Ctrl]);
 
     function Dashboard2Ctrl($scope, $http, $state, $stateParams, appSettings, popupService) {
-        $http.post(appSettings.serverPath + "/spark/percentoptionoffactor", { modelId: "19028285-5fd1-40d7-be66-5e630f948ee5" })
-            .success(function (data) {
-                $scope.chartFt = new CanvasJS.Chart("chartFtOtpINFt",
-                    {
-                        theme: 'theme2',
-                        title:{
-                            text: data.PercentOptionOfFactor.body.modelName
+        $http.post(appSettings.serverPath + "/modelinfo/getbymodelinfostatus", { status: 'publish' })
+            .success(function (data, status, headers, config) {
+                //console.log(data);
+                $scope.models = data.getModelInfoByStatusJSON.body;
 
-                        },
-                        toolTip:{
-                            content: "{name} : #percent%"
-                        },
-                        animationEnabled: true,
-                        axisY:{
-                            title: "percent"
-                        },
-                        data:data.PercentOptionOfFactor.body.data
+                if ($scope.choiceModel != '') {
+                    $scope.modelChanged($scope.choiceModel);
+                }
+            })
+            .error(function (data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
 
+        $scope.modelChanged = function (id) {
+            //console.log(id);
+            for (var i = 0; i < $scope.models.length; i++) {
+                if ($scope.models[i]._id == id) {
+                    $scope.model = $scope.models[i];
+                    //console.log($scope.model.min);
+                }
+            }
+            if (id != null) {
+                $http.post(appSettings.serverPath + "/spark/percentoptionoffactor", { modelId: "19028285-5fd1-40d7-be66-5e630f948ee5" })
+                    .success(function (data) {
+                        $scope.chartFt = new CanvasJS.Chart("chartFtOtpINFt",
+                            {
+                                theme: 'theme2',
+                                title:{
+                                    text: data.PercentOptionOfFactor.body.modelName
+
+                                },
+                                toolTip:{
+                                    content: "{name} : #percent%"
+                                },
+                                animationEnabled: true,
+                                axisY:{
+                                    title: "percent"
+                                },
+                                data:data.PercentOptionOfFactor.body.data
+
+                            });
+
+                        $scope.chartFt.render();
                     });
+            }else {
 
-                $scope.chartFt.render();
-        });
+            }
+        }
+
 
     }
 
--- FrontEnd/app/views/dashboard/dashboarCtrl.js
+++ FrontEnd/app/views/dashboard/dashboarCtrl.js
@@ -8,80 +8,167 @@
 
     function DashboardCtrl($scope, $http, $state, $stateParams, appSettings, popupService) {
 
-        $scope.bars = {};
-
-        $http.post(appSettings.serverPath + "/spark/scoringrange", { modelId: "19028285-5fd1-40d7-be66-5e630f948ee5" })
-            .success(function (data) {
-                $scope.bars = data.ScoringRange.body;
-                var sumCount = 0;
-                for(var i = 1; i<$scope.bars.length; i++){
-                    sumCount = parseInt(sumCount) + parseInt($scope.bars[i].application_count)
-                }
-                var dataPoints = [];
-                for(var i = 1; i<$scope.bars.length; i++){
-                    dataPoints.push({
-                        label: $scope.bars[i].rating_code,
-                        y: (parseInt($scope.bars[i].application_count)/parseInt(sumCount))*100
-                    });
-                }
-                var dataPointsPie = [];
-                for(var i = 1; i<$scope.bars.length; i++){
-                    dataPointsPie.push({
-                        label: $scope.bars[i].rating_code,
-                        y: $scope.bars[i].application_count
-                    });
-                }
-                $scope.chart = new CanvasJS.Chart("chartContainer", {
-                    animationEnabled: true,
-                    theme: 'theme1',
-                    title:{
-                        text: "Scoring Range Chart (" + $scope.bars[0].modelName + ")"
-                    },
-                    axisY: {
-                        title: "percent"
-                    },
-                    data: [
-                        {
-                            type: "column",
-                            dataPoints: dataPoints
-                        }
-                    ]
-                });
-
-                $scope.chart.render(); //render the chart for the first time
-
-                $scope.chartPie = new CanvasJS.Chart("chartPie", {
-                    animationEnabled: true,
-                    theme: 'theme1',
-                    title:{
-                        text: ""
-                    },
-                    data: [
-                        {
-                            type: "pie",
-                            dataPoints: dataPointsPie
-                        }
-                    ]
-                });
-
-                $scope.chartPie.render(); //render the chart for the first time
-            });
+        //$scope.bars = {};
 
         $scope.topbot1T = {}
         $scope.topbot1B = {}
-        $http.post(appSettings.serverPath + "/spark/topbot", { factorOptionId: "1a021ec7-83af-46ff-af31-70ba8600ff77" })
-            .success(function (data) {
-                $scope.topbot1T = data.ScoringRange.body[0].Top
-                $scope.topbot1B = data.ScoringRange.body[1].Bot
-            });
+
 
         $scope.topbot2T = {}
         $scope.topbot2B = {}
-        $http.post(appSettings.serverPath + "/spark/topbot", { factorOptionId: "9daa9841-c94a-498d-9a32-28cd8f91ec80" })
-            .success(function (data) {
-                $scope.topbot2T = data.ScoringRange.body[0].Top
-                $scope.topbot2B = data.ScoringRange.body[1].Bot
+
+        $http.post(appSettings.serverPath + "/modelinfo/getbymodelinfostatus", { status: 'publish' })
+            .success(function (data, status, headers, config) {
+                //console.log(data);
+                $scope.models = data.getModelInfoByStatusJSON.body;
+
+                if ($scope.choiceModel != '') {
+                    $scope.modelChanged($scope.choiceModel);
+                }
+            })
+            .error(function (data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
             });
+        $scope.option1Changed = function (id) {
+            //console.log(id);
+            for (var i = 0; i < $scope.factorOption1s.length; i++) {
+                if ($scope.factorOption1s[i].FactorOptionId == id) {
+                    $scope.factorOption1 = $scope.factorOption1s[i];
+                    //console.log($scope.model.min);
+                }
+            }
+            if(id != null) {
+                $http.post(appSettings.serverPath + "/spark/topbot", {factorOptionId: id})
+                    .success(function (data) {
+                        if(data.ScoringRange.body!=null) {
+                            $scope.topbot1T = data.ScoringRange.body[0].Top
+                            $scope.topbot1B = data.ScoringRange.body[1].Bot
+                        }
+                    });
+            }
+        }
+
+        $scope.option2Changed = function (id) {
+            //console.log(id);
+            for (var i = 0; i < $scope.factorOption2s.length; i++) {
+                if ($scope.factorOption2s[i].FactorOptionId == id) {
+                    $scope.factorOption2 = $scope.factorOption2s[i];
+                    //console.log($scope.model.min);
+                }
+            }
+            if(id != null) {
+                $http.post(appSettings.serverPath + "/spark/topbot", {factorOptionId: id})
+                    .success(function (data) {
+                        if(data.ScoringRange.body!=null) {
+                            $scope.topbot2T = data.ScoringRange.body[0].Top
+                            $scope.topbot2B = data.ScoringRange.body[1].Bot
+                        }
+                    });
+            }
+        }
+
+        $scope.modelChanged = function (id) {
+            //console.log(id);
+            for (var i = 0; i < $scope.models.length; i++) {
+                if ($scope.models[i]._id == id) {
+                    $scope.model = $scope.models[i];
+                    //console.log($scope.model.min);
+                }
+            }
+
+            if (id != null) {
+
+                $http.post(appSettings.serverPath + "/factoroption/getoptionbymodelid", { modelid: id })
+                    .success(function (data, status, headers, config) {
+                        //console.log(data);
+                        $scope.factorOption1s = data.SUCCESS;
+
+                        if ($scope.choiceOptl != '') {
+                            $scope.option1Changed($scope.choiceOptl);
+                        }
+                    })
+                    .error(function (data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+
+                $http.post(appSettings.serverPath + "/factoroption/getoptionbymodelid", { modelid: id })
+                    .success(function (data, status, headers, config) {
+                        //console.log(data);
+                        $scope.factorOption2s = data.SUCCESS;
+
+                        if ($scope.choiceOpt2 != '') {
+                            $scope.option2Changed($scope.choiceOpt2);
+                        }
+                    })
+                    .error(function (data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+
+                $http.post(appSettings.serverPath + "/spark/scoringrange", {modelId: id})
+                    .success(function (data) {
+                        if (data.ScoringRange.body != null) {
+                            $scope.bars = data.ScoringRange.body;
+                            var sumCount = 0;
+                            for (var i = 1; i < $scope.bars.length; i++) {
+                                sumCount = parseInt(sumCount) + parseInt($scope.bars[i].application_count)
+                            }
+                            var dataPoints = [];
+                            for (var i = 1; i < $scope.bars.length; i++) {
+                                dataPoints.push({
+                                    label: $scope.bars[i].rating_code,
+                                    y: (parseInt($scope.bars[i].application_count) / parseInt(sumCount)) * 100
+                                });
+                            }
+                            var dataPointsPie = [];
+                            for (var i = 1; i < $scope.bars.length; i++) {
+                                dataPointsPie.push({
+                                    label: $scope.bars[i].rating_code,
+                                    y: $scope.bars[i].application_count
+                                });
+                            }
+                            $scope.chart = new CanvasJS.Chart("chartContainer", {
+                                animationEnabled: true,
+                                theme: 'theme1',
+                                title: {
+                                    text: "Scoring Range Chart (" + $scope.bars[0].modelName + ")"
+                                },
+                                axisY: {
+                                    title: "percent"
+                                },
+                                data: [
+                                    {
+                                        type: "column",
+                                        dataPoints: dataPoints
+                                    }
+                                ]
+                            });
+
+                            $scope.chart.render(); //render the chart for the first time
+
+                            $scope.chartPie = new CanvasJS.Chart("chartPie", {
+                                animationEnabled: true,
+                                theme: 'theme1',
+                                title: {
+                                    text: ""
+                                },
+                                data: [
+                                    {
+                                        type: "pie",
+                                        dataPoints: dataPointsPie
+                                    }
+                                ]
+                            });
+
+                            $scope.chartPie.render(); //render the chart for the first time
+                        }
+                    });
+            }else {
+
+            }
+        }
 
     }
 
